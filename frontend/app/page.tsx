233c3bc076dfbd349e3f4eb5778051d335a812e3
Matlavv <mathis.laversin92@gmail.com>
2025-01-25 20:08:01 +0100

    setup componentes

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,6 +1,8 @@
 import { Header } from '@/app/components/layout/Header';
 import SectionSearchBar from '@/app/components/layout/SectionSearchBar';
 import Description from './components/layout/Description';
+import SectionNumber from './components/layout/SectionNumber';
+import SectionCards from './components/layout/SectionCards';
 
 export default function Home() {
   return (
@@ -9,6 +11,8 @@ export default function Home() {
         <Header />
         <SectionSearchBar />
         <Description />
+        <SectionNumber />
+        <SectionCards />
       </section>
     </div>
   );
