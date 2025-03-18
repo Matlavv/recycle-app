4ef78245e37301704aaf978ca12d96ae39d99193
Matlavv <mathis.laversin92@gmail.com>
2025-03-18 15:07:01 +0100

    bento update

--- frontend/app/components/ui/Bento.tsx
+++ frontend/app/components/ui/Bento.tsx
@@ -1,4 +1,8 @@
 import React from 'react';
+import Image from 'next/image';
+import leaves from '../../assets/leaves.png';
+import jardin from '../../assets/jardin.jpg';
+import bee from '../../assets/bee.jpg';
 
 export default function Bento() {
   return (
@@ -6,43 +10,17 @@ export default function Bento() {
       <div className="grid sm:grid-cols-2 sm:grid-rows-2 gap-3 sm:gap-4">
         {/* Grande carte à gauche */}
         <div className="relative col-span-1 row-span-2 overflow-hidden rounded-2xl bg-light-yellow shadow-sm">
-          <div className="p-4">
-            <h3 className="text-sm font-medium text-dark-green font-urbanist">
-              Mobile friendly
-            </h3>
-            <p className="mt-1 text-sm text-dark-green font-urbanist line-clamp-2">
-              Anim aute id magna aliqua ad ad non deserunt sunt.
-            </p>
-          </div>
-          <div className="p-2">
-            <div className="h-52 w-full rounded-lg bg-light-green/20"></div>
-          </div>
+          <Image src={leaves} alt="leaves" className="w-full h-full object-cover" />
         </div>
 
         {/* Carte en haut à droite */}
-        <div className="relative overflow-hidden rounded-2xl bg-dark-green p-4 shadow-sm">
-          <h3 className="text-sm font-medium text-light-yellow font-urbanist">
-            Performance
-          </h3>
-          <p className="mt-1 text-sm text-light-yellow font-urbanist line-clamp-2">
-            Lorem ipsum, dolor sit amet consectetur.
-          </p>
-          <div className="absolute bottom-0 right-0 p-2">
-            <div className="h-12 w-12 rounded-lg bg-light-green/20"></div>
-          </div>
+        <div className="relative overflow-hidden rounded-2xl shadow-sm">
+          <Image src={jardin} alt="jardin" className="w-full h-full object-cover" />
         </div>
 
         {/* Carte en bas à droite */}
-        <div className="relative overflow-hidden rounded-2xl bg-grey p-4 shadow-sm">
-          <h3 className="text-sm font-medium text-dark font-urbanist">
-            Security
-          </h3>
-          <p className="mt-1 text-sm text-dark font-urbanist line-clamp-2">
-            Morbi viverra dui mi arcu sed tellus.
-          </p>
-          <div className="absolute bottom-0 right-0 p-2">
-            <div className="h-12 w-12 rounded-lg bg-light-green/20"></div>
-          </div>
+        <div className="relative overflow-hidden rounded-2xl shadow-sm">
+          <Image src={bee} alt="jardin" className="w-full h-full object-cover" />
         </div>
       </div>
     </div>
